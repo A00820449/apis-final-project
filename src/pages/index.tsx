e70381aa6cffc4c45dc927c5f84bcd4e135d84f9
Miguel Tornero <a00820449@itesm.mx>
2023-06-01 10:35:54 -0600

    fixed frontpage font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Button, Container, Typography } from '@mui/material'
+import { Button, Container, Typography, useMediaQuery } from '@mui/material'
 import Link from 'next/link'
 
 export default function Home() {
+  const match = useMediaQuery("(max-width:600px)")
   return (
     <Container className='main' sx={{textAlign: "center"}}>
-      <Typography variant='h1'>Welcome!</Typography>
+      <Typography variant='h1' sx={match ? {fontSize: "50px"} : {}}>Welcome!</Typography>
       <Button LinkComponent={Link} href={'/dashboard'} variant="contained">
         Enter App
       </Button>
