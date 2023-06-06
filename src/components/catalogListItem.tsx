4c186b74218e86842f3ab9d13b83a53443f5b2ec
Miguel Tornero <a00820449@itesm.mx>
2023-06-05 23:40:23 -0600

    finished service delete

--- src/components/catalogListItem.tsx
+++ src/components/catalogListItem.tsx
@@ -1,4 +1,4 @@
-import { useDeleteService } from "@/lib/hooks"
+import { useCatalog, useDeleteService } from "@/lib/hooks"
 import { Button, ListItem } from "@mui/material"
 import { Service } from "@prisma/client"
 import { Delete as DeleteIcon } from "@mui/icons-material"
@@ -9,11 +9,18 @@ type CatalogItemProps = {
 
 export default function CatalogListItem({service}: CatalogItemProps) {
     const { trigger, isMutating } = useDeleteService({serviceID: service.id})
+    const { mutate } = useCatalog()
+
+    const handleClick = async () => {
+        await trigger()
+        mutate()
+    }
+
     return (
     <ListItem sx={{display: "block", backgroundColor: "rgba(0,0,0,0.1)", margin: "1rem 0", borderRadius: "5px", padding: "1rem"}}>
         <div style={{fontWeight: "bold"}}>{service.eventName}</div>
         <div style={{margin: "1rem 0"}}>{service.description ? `Description: ${service.description}`: "No description."}</div>
-        <Button variant="contained" color="error">
+        <Button variant="contained" color="error" disabled={isMutating} onClick={handleClick}>
             <DeleteIcon/>
         </Button>
     </ListItem>
